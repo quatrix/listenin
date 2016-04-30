84d545fa24e78bdc3ee1c4a05995e0b89b69ef00
quatrix <evil.legacy@gmail.com>
2016-04-30 20:40:57 +0000

    timeout while posting

--- loop.py
+++ loop.py
@@ -125,7 +125,7 @@ class ListenIn(object):
 
     def upload_sample(self, sample):
         url = 'http://api.listenin.io/upload/{}/'.format(self.boxid)
-        requests.post(url, data=sample).raise_for_status()
+        requests.post(url, data=sample, timeout=60).raise_for_status()
 
     def cleanup(self):
         self.stop_blinker()
