13efe9cafaa2b680e43e96342292ea1a40376893
root <root@mimosabox.com>
2016-05-10 05:16:51 -0400

    es returns a json, lolz

--- backend/health_handler.py
+++ backend/health_handler.py
@@ -3,6 +3,7 @@ from tornado.gen import coroutine, Return
 from base_handler import BaseHandler
 from collections import defaultdict
 import logging
+import json
 
 es_query = """
 {{
@@ -39,45 +40,46 @@ es_query = """
 }}
 """
 
-@coroutine
-def get_last_message(es_host, host, message):
-    query = es_query.format(host=host, message=message)
-    http_client = AsyncHTTPClient()
-    res = yield http_client.fetch(
-        "{}/_search".format(es_host),
-         method='GET',
-         body=query,
-         allow_nonstandard_methods=True
-    )
-
-    if res['hits']['total'] == 0:
-        raise RuntimeError('query returned zero results')
-
-    raise Return(res['hits']['hits'][0])
 
 class HealthHandler(BaseHandler):
     @coroutine
+    def get_last_message(self, host, message):
+        query = es_query.format(host=host, message=message)
+        http_client = AsyncHTTPClient()
+        res = yield http_client.fetch(
+            "{}/_search".format(self.settings['elasticsearch_host']),
+             method='GET',
+             body=query,
+             allow_nonstandard_methods=True
+        )
+
+        res = json.loads(res.body)
+
+        if res['hits']['total'] == 0:
+            raise RuntimeError('query returned zero results')
+
+        raise Return(res['hits']['hits'][0])
+
+    @coroutine
     def get(self):
         res = defaultdict(dict)
-        es_host = self.settings['elasticsearch_host']
 
         for box in 'listenin-radio', 'listenin-pasaz':
             try:
-                last_blink = yield get_last_message(es_host, box, 'INFO:root:blink')
+                last_blink = yield self.get_last_message(box, 'INFO:root:blink')
                 res[box]['last_blink'] = last_blink['_source']['@timestamp']
             except Exception:
                 logging.exception('get last blink')
             
             try:
-                last_color_change = yield get_last_message(
-                    es_host,
+                last_color_change = yield self.get_last_message(
                     box,
                     'INFO:root:setting led color to'
                 )
 
                 res[box]['last_color'] = {
-                    'color': last_blink['_source']['message'].split()[-1],
-                    'changed_at': last_blink['_source']['@timestamp']
+                    'color': last_color_change['_source']['message'].split()[-1],
+                    'changed_at': last_color_change['_source']['@timestamp']
                 }
             except Exception:
                 logging.exception('get last color')
