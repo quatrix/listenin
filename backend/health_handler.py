56a8ea97c6ee27cbd3efd196eeb4905e0c335b1a
quatrix <evil.legacy@gmail.com>
2016-05-21 11:04:32 +0300

    health cleanup

--- backend/health_handler.py
+++ backend/health_handler.py
@@ -1,4 +1,4 @@
-from tornado.gen import coroutine
+from tornado.gen import coroutine, Return
 from base_handler import BaseHandler
 from collections import defaultdict
 
@@ -8,52 +8,57 @@ import json
 
 class HealthHandler(BaseHandler):
     @coroutine
+    def get_last_blink(self, box):
+        try:
+            last_blink = yield self.settings['es'].get_last_document(
+                host=box,
+                message='INFO:root:blink'
+            )
+
+            raise Return(last_blink['_source']['@timestamp'])
+        except Exception:
+            logging.exception('get last blink')
+            raise Return(None)
+
+    @coroutine
+    def get_last_color(self, box):
+        try:
+            last_color_change = yield self.settings['es'].get_last_document(
+                host=box,
+                message='INFO:root:setting led color to'
+            )
+
+            raise Return({
+                'color': last_color_change['_source']['message'].split()[-1],
+                'time': last_color_change['_source']['@timestamp']
+            })
+        except Exception:
+            logging.exception('get last color')
+            raise Return({'color': None, 'time': None})
+
+    @coroutine
+    def get_last_upload(self, box):
+        try:
+            last_upload = yield self.settings['es'].get_last_document(
+                boxid=box.split('-')[1],
+                message='success'
+            )
+
+            raise Return({
+                'time': last_upload['_source']['@timestamp'],
+            })
+
+        except Exception:
+            logging.exception('get last upload')
+            raise Return({'time': None})
+
+    @coroutine
     def get(self):
         res = defaultdict(dict)
-        es = self.settings['es']
 
         for box in 'listenin-radio', 'listenin-pasaz':
-            try:
-                last_blink = yield es.get_last_document(host=box, message='INFO:root:blink')
-                res[box]['last_blink'] = last_blink['_source']['@timestamp']
-            except Exception:
-                logging.exception('get last blink')
-            
-            try:
-                last_color_change = yield es.get_last_document(
-                    host=box,
-                    message='INFO:root:setting led color to'
-                )
-
-                res[box]['last_color'] = {
-                    'color': last_color_change['_source']['message'].split()[-1],
-                    'time': last_color_change['_source']['@timestamp']
-                }
-            except Exception:
-                logging.exception('get last color')
-
-            try:
-                last_upload = yield es.get_last_document(
-                    boxid=box.split('-')[1],
-                    message='success'
-                )
-
-                song = last_upload.get('acrcloud')
-
-                if song is not None:
-                    song = {
-                        'album': song.get('album'),
-                        'genres': ' '.join(song.get('genres')),
-                        'artists': ' '.join(song.get('artists')),
-                        'title': song.get('title')
-                    }
-
-                res[box]['last_upload'] = {
-                    'time': last_upload['_source']['@timestamp'],
-                    'song': song,
-                }
-
-            except Exception:
-                logging.exception('get last upload')
+            res[box]['last_blink'] = yield self.get_last_blink(box)
+            res[box]['last_color'] = yield self.get_last_color(box)
+            res[box]['last_upload'] = yield self.get_last_upload(box)
 
         self.finish(res)
