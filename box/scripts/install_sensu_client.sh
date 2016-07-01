f97d7cfdf499e6d6dc91fcf1463d285ad20e4c7d
quatrix <evil.legacy@gmail.com>
2016-07-01 10:01:27 +0000

    Automate install sensu client

--- box/scripts/install_sensu_client.sh
+++ box/scripts/install_sensu_client.sh
@@ -2,14 +2,17 @@
 
 set -e 
 
+export LC_ALL=C
+BOX_NAME=$(hostname | cut -d- -f2)
 source /home/pi/listenin/box/.pyenv/bin/activate
 pip install -r /home/pi/listenin/box/requirements.txt
 
 wget -q http://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | sudo apt-key add -
 echo "deb     http://sensu.global.ssl.fastly.net/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list
-apt-get update
-apt-get install sensu
-cp -r /home/pi/listenin/box/etc/sensu /etc/
+sudo apt-get update
+sudo apt-get install sensu
+sudo cp -r /home/pi/listenin/box/etc/sensu /etc/
+sudo perl -p -i -e "s/BOX_NAME/${BOX_NAME}/g" /etc/sensu/conf.d/client.json
 
-update-rc.d sensu-client defaults
-/etc/init.d/sensu-client restart
+sudo update-rc.d sensu-client defaults
+sudo /etc/init.d/sensu-client restart
