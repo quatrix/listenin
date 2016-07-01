ffa320acb6e46085bfe178b219b5afad3e114215
quatrix <evil.legacy@gmail.com>
2016-07-01 11:01:40 +0300

    Add sensu

---
+++ box/scripts/install_sensu_client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e 
+
+wget -q http://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | sudo apt-key add -
+echo "deb     http://sensu.global.ssl.fastly.net/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list
+sudo apt-get update
+sudo apt-get install sensu
+
+cp -r /root/listenin/box/etc/sensu /etc/
