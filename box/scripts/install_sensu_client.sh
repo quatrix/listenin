cde8639fcf7aed094d0edd30e0fbd65afe0184e2
miedwar meshbesher <eddie@listenin.io>
2016-07-01 09:43:59 +0000

    install sensu client installs req.txt

--- box/scripts/install_sensu_client.sh
+++ box/scripts/install_sensu_client.sh
@@ -2,9 +2,14 @@
 
 set -e 
 
+source /home/pi/listenin/box/.pyenv/bin/activate
+pip install -r /home/pi/listenin/box/requirements.txt
+
 wget -q http://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | sudo apt-key add -
 echo "deb     http://sensu.global.ssl.fastly.net/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list
-sudo apt-get update
-sudo apt-get install sensu
-
+apt-get update
+apt-get install sensu
 cp -r /home/pi/listenin/box/etc/sensu /etc/
+
+update-rc.d sensu-client defaults
+/etc/init.d/sensu-client restart
