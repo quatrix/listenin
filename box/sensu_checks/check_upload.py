4c51f1172c1f140d19fc66d12a80ce3c1fcc2949
quatrix <evil.legacy@gmail.com>
2016-07-04 16:40:02 +0300

    check_health -> check_upload

---
+++ box/sensu_checks/check_upload.py
@@ -0,0 +1,73 @@
+"""
+check upload script
+
+"""
+from __future__ import print_function
+
+import sys
+from datetime import datetime
+import argparse
+import requests
+from dateutil import parser
+from dateutil.tz import tzutc
+from humanize import naturaltime
+
+OK = 0
+CRITICAL = 2
+UNKNOWN = 2
+
+MINUTE = 60
+API_URL = 'http://api.listenin.io/health'
+
+
+def check_upload(box_name, upload_threshold):
+    """Checks last upload time for box_name and returns Nagios"""
+
+    health = requests.get(API_URL).json()
+    box_name = 'listenin-' + box_name
+
+    box = health[box_name]
+    last_blink = box['last_blink']
+    last_upload = box['last_upload']['time']
+
+    if last_blink is None:
+        print('{} last blinked more than two days'.format(box_name))
+        return CRITICAL
+
+    if last_upload is None:
+        print('{} last uploaded more than two days'.format(box_name))
+        return CRITICAL
+
+    now = datetime.now(tzutc())
+    last_upload = now - parser.parse(last_upload)
+    last_blink = now - parser.parse(last_blink)
+
+    if last_blink.total_seconds() > 10 * MINUTE:
+        print('{} last blinked {}'.format(box_name, naturaltime(last_blink)))
+        return CRITICAL
+
+    if last_upload.total_seconds() > upload_threshold:
+        print('{} last uploaded {}'.format(box_name, naturaltime(last_upload)))
+        return CRITICAL
+
+    print('{} last upload {}'.format(box_name, naturaltime(last_upload)))
+    return OK
+
+
+def main():
+    """
+    main entry point
+    """
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('box_name', help='box name')
+    arg_parser.add_argument('--upload-threshold', type=int, help='threshold (minutes)', default=10)
+    args = arg_parser.parse_args()
+
+    return check_upload(
+        box_name=args.box_name,
+        upload_threshold=args.upload_threshold * MINUTE
+    )
+
+if __name__ == '__main__':
+    sys.exit(main())
