78510158a948e295b4f7ac0f6b044fca631d8c94
quatrix <evil.legacy@gmail.com>
2016-07-07 00:48:53 +0300

    Update sensu deploy script

--- box/scripts/update_config.sh
+++ box/scripts/update_config.sh
@@ -3,13 +3,9 @@
 set -e
 export LC_ALL=C
 
-echo "killing sensu"
-sudo killall sensu-client
-sudo update-rc.d sensu-client remove -f
+echo "copying sensu config"
+sudo cp -r /home/pi/listenin/box/etc/systemd/system/sensu-client.service /etc/systemd/system/
 
 echo "enabling sensu systemd"
-sudo cp -r /home/pi/listenin/box/etc/systemd/system/sensu-client.service /etc/systemd/system/
 sudo systemctl enable sensu-client
-
-echo "starting sensu"
-sudo systemctl start sensu-client
+sudo systemctl restart sensu-client
