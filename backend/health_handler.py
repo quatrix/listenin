52d7f44ccba97feb7109bebb50fa6b7a643babcc
root <root@mimosabox.com>
2016-07-23 16:25:23 -0400

    health takes a box argument

--- backend/health_handler.py
+++ backend/health_handler.py
@@ -3,9 +3,12 @@ from collections import defaultdict
 
 from tornado.gen import coroutine, Return
 from base_handler import BaseHandler
+from ttldict import TTLDict
 
 
 class HealthHandler(BaseHandler):
+    _health_cache = TTLDict(60)
+
     @coroutine
     def get_last_blink(self, box):
         try:
@@ -13,25 +16,11 @@ class HealthHandler(BaseHandler):
                 host=box,
                 message='INFO:root:blink'
             )
-        except Exception:
+        except Exception as e:
+            print(e)
             raise Return(None)
 
         raise Return(last_blink['_source']['@timestamp'])
-
-    @coroutine
-    def get_last_color(self, box):
-        try:
-            last_color_change = yield self.settings['es'].get_last_document(
-                host=box,
-                message='INFO:root:setting led color to'
-            )
-        except Exception:
-            raise Return({'color': None, 'time': None})
-
-        raise Return({
-            'color': last_color_change['_source']['message'].split()[-1],
-            'time': last_color_change['_source']['@timestamp'],
-        })
 
     @coroutine
     def get_last_upload(self, box):
@@ -43,19 +32,25 @@ class HealthHandler(BaseHandler):
         except Exception:
             raise Return({'time': None})
 
-        raise Return({
-            'time': last_upload['_source']['@timestamp'],
-        })
+        raise Return(last_upload['_source']['@timestamp'])
 
-    #FIXME should cache this
+    @coroutine
+    def _get_box_health(self, box):
+        box = 'listenin-' + box
+
+        if box not in self._health_cache:
+            last_blink = yield self.get_last_blink(box)
+            last_upload = yield self.get_last_upload(box)
+
+            self._health_cache[box] = {
+                'last_blink': last_blink,
+                'last_upload': last_upload,
+            }
+
+        raise Return(self._health_cache[box])
+
     @coroutine
     def get(self):
-        res = defaultdict(dict)
-
-        for box in 'radio', 'pasaz', 'annaloulou', 'limalima', 'rothschild12', 'hostel51', 'kulialma':
-            box = 'listenin-' + box
-            res[box]['last_blink'] = yield self.get_last_blink(box)
-            res[box]['last_color'] = yield self.get_last_color(box)
-            res[box]['last_upload'] = yield self.get_last_upload(box)
-
-        self.finish(res)
+        box = self.get_argument('box')
+        health = yield self._get_box_health(box)
+        self.finish({box: health})
