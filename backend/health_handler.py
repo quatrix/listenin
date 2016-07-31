a7ad8d34371730eb30de6d6b78abf145fd3c3ca1
quatrix <evil.legacy@gmail.com>
2016-07-31 15:20:20 +0300

    Add box=all option to /health api endpoint

--- backend/health_handler.py
+++ backend/health_handler.py
@@ -3,11 +3,23 @@ from base_handler import BaseHandler
 
 class HealthHandler(BaseHandler):
     def get_last_upload(self, box):
-        return self.settings['samples'].all()[box][0]['date']
+        try:
+            return self.settings['samples'].all()[box][0]['date']
+        except IndexError:
+            return None
+
+    def get_all_last_upload(self):
+        return {
+            box: {'last_upload': self.get_last_upload(box)}
+            for box in self.settings['samples'].all().keys()
+        }
 
     def get(self):
         box = self.get_argument('box')
 
-        self.finish({
-            'last_upload': self.get_last_upload(box)
-        })
+        if box == 'all':
+            self.finish(self.get_all_last_upload())
+        else:
+            self.finish({
+                'last_upload': self.get_last_upload(box)
+            })
