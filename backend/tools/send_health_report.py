f0f7258b8450522119e57d9d9c014dd2d820b3ed
quatrix <evil.legacy@gmail.com>
2016-08-01 20:22:20 +0300

    Send health report

--- backend/tools/send_health_report.py
+++ backend/tools/send_health_report.py
@@ -9,6 +9,12 @@ from dateutil import tz, parser
 from humanize import naturaltime
 
 
+_mailgun = {
+    'key': 'key-32c14a9ca66a0d37282d27ac739dc493',
+    'sandbox': 'mg.listenin.io',
+}
+
+
 _hours = {
     'radio': {
         '_tz': 'Israel',
@@ -55,6 +61,17 @@ _hours = {
 }
 
 
+def send_report(subject, body, recipient):
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(_mailgun['sandbox'])
+    request = requests.post(request_url, auth=('api', _mailgun['key']), data={
+        'from': 'no-reply@listenin.io',
+        'to': recipient,
+        'subject': subject,
+        'text': body,
+    })
+    print(request)
+
+
 def _get_closing_hour(venue):
     day = datetime.datetime.today()
 
@@ -88,8 +105,16 @@ def _utc_to_localtime(d, local_tz):
     return utc.astimezone(tz.gettz(local_tz))
 
 
+def today_at(local_tz):
+    utc = datetime.datetime.utcnow()
+    utc = utc.replace(tzinfo=tz.gettz('UTC'))
+    return utc.astimezone(tz.gettz(local_tz))
+
 def main():
     health = _get_health()
+
+    recipients = ['evil.legacy@gmail.com']
+    report = []
 
     for box_name, box in health.iteritems():
         expected = _get_closing_hour(box_name)
@@ -98,16 +123,23 @@ def main():
         if actual < expected:
             last_upload = datetime.datetime.now(tz.tzutc()) - parser.parse(box['last_upload'])
 
-            extra_info = ' (expected: {})'.format(expected.strftime('%m/%d %H:%M%z'))
+            extra_info = ' (expected: {})'.format(expected.strftime('%m/%d %H:%M'))
 
             if last_upload.total_seconds() > 60 * 60 * 24:
                 extra_info = ' ({})'.format(naturaltime(last_upload))
 
-            print('{} last sample at {}{}'.format(
+            report.append('{} last sample at {}{}'.format(
                 box_name,
-                actual.strftime('%m/%d %H:%M%z'),
+                actual.strftime('%m/%d %H:%M'),
                 extra_info,
             ))
 
+    
+    for recpt in recipients:
+        send_report(
+            subject='ListenIn health status for {}'.format(today_at('Israel').strftime('%m/%d')),
+            body='\n'.join(report),
+            recipient=recpt
+        )
 if __name__ == '__main__':
     main()
