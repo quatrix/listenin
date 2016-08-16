3d42d1c5b8d47185859d63dc111fbd0489aacf95
quatrix <evil.legacy@gmail.com>
2016-08-16 19:44:57 +0300

    More robust gracenote_identify.py

--- backend/tools/gracetune_identify.py
+++ backend/tools/gracetune_identify.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
-import pygn
-import click
+import time
+import tempfile
 import subprocess
 import json
-import tempfile
+
+import click
+import pygn
 
 @click.command()
 @click.option('--client-id', required=True)
@@ -25,23 +28,44 @@ def main(client_id, user_id, license, filename):
     with tempfile.NamedTemporaryFile(suffix='.wav') as wav:
         subprocess.check_call(['sox', filename, '-r', '44100', wav.name])
         c_id, tag_id = client_id.split('-')
-        res = subprocess.check_output(['gracetune_musicid_stream', c_id, tag_id, license, 'online', wav.name])
-        res = json.loads(res)
+        res = subprocess.Popen([
+            'gracetune_musicid_stream',
+            c_id,
+            tag_id,
+            license,
+            'online',
+            wav.name
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        stdout, stderr = res.communicate()
+
+        try:
+            res = json.loads(stdout)
+        except Exception:
+            print(json.dumps({'error': stderr}, indent=4))
+            return
 
     if 'error' in res:
         print(json.dumps(res, indent=4))
         return
 
-    metadata = pygn.search(
-        clientID=client_id,
-        userID=user_id,
-        artist=res['artist'], 
-        album=res['album'],
-        track=res['track'],
-    )
+    for _ in xrange(5):
+        try:
+            metadata = pygn.search(
+                clientID=client_id,
+                userID=user_id,
+                artist=res['artist'],
+                album=res['album'],
+                track=res['track'],
+            )
 
-    print(json.dumps(metadata, indent=4))
+            if metadata:
+                print(json.dumps(metadata, indent=4))
+                return
+        except Exception:
+            time.sleep(0.5)
 
+    print(json.dumps({'error': 'failed to fetch song details via pygn'}, indent=4))
 
 if __name__ == '__main__':
     main()
