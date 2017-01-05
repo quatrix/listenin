e59a7660f691db87c240ac40214ef9a082ed28ca
quatrix <evil.legacy@gmail.com>
2017-01-05 21:48:04 +0200

    don't record if signal less then -35db

--- box/loop.py
+++ box/loop.py
@@ -105,7 +105,7 @@ class ListenIn(object):
             if os.path.exists(f):
                 os.unlink(f)
 
-        rec_cmd = 'rec --no-show-progress -t mp3 -C 0 {} silence 1 0.1 0.05% 1 2.0 0.05% trim 0 {}'.format(
+        rec_cmd = 'rec --no-show-progress -t mp3 -C 0 {} silence 1 0.1 -35d 1 2.0 -35d trim 0 {}'.format(
             output_file,
             self.duration,
         )
